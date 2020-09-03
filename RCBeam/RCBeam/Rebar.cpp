eb766283fb3c494c876677fe2b718977773b40fd
M Jasser-Stone <stone.ma@gmail.com>
2020-09-03 16:07:08 -0400

    Reworked rebar files. Removed enum and simplified to int based barsize. Will have to add error handling later.

--- RCBeam/RCBeam/Rebar.cpp
+++ RCBeam/RCBeam/Rebar.cpp
@@ -1,36 +1,21 @@
 #include "Rebar.h"
 
-Rebar::Rebar()
+Rebar::Rebar(Steel* pSteeltype, int barsize)
 {
-
+	// Set Steel Type
+	m_pSteel = pSteeltype;
+	// Set Barsize
+	SetBarSize(barsize);
 }
 
-void Rebar::SetGeometry(USRebarSize barsize)
+void Rebar::SetGeometry()
 {
-	double diameter = 0.375;
-	switch (barsize)
-	{
-		case (USRebarSize::No3):
-			double diameter = 0.375;
-			break;
-		case USRebarSize::No4:
-			diameter = 0.500;
-			break;
-		case USRebarSize::No5:
-			diameter = 0.625;
-			break;
-		case USRebarSize::No6:
-			diameter = 0.750;
-			break;
-		case USRebarSize::No7:
-			diameter = 0.875;
-			break;
-		case USRebarSize::No8:
-			diameter = 1.00;
-			break;
-		default:
-			break;
-	}
+	double diameter = (double)m_BarSize / 8.0;
 	SetDiameter(diameter);
 	SetArea(diameter);
+}
+
+void Rebar::SetBarSize(int barsize)
+{
+	m_BarSize = barsize;
 }
--- RCBeam/RCBeam/Rebar.h
+++ RCBeam/RCBeam/Rebar.h
@@ -6,30 +6,21 @@ class Rebar
 {
 protected:
 	double m_Diameter, m_Area;
-
-	enum class USRebarSize {
-		No3 = 3,
-		No4,
-		No5,
-		No6,
-		No7,
-		No8,
-		No9
-	};
-
-	USRebarSize m_BarSize;
+	int m_BarSize;
 
 	// Steel type
 	Steel* m_pSteel;
 
 public:
 	// Constructors
-	Rebar(Steel& steeltype, USRebarSize barsize);
+	Rebar(Steel* pSteeltype, int barsize);
 
 	// sets
 	void SetDiameter(double diameter) { m_Diameter = diameter; }
 	void SetArea(double diameter) { m_Area = 3.14159 * pow(diameter, 2) / 4; }
-	void SetGeometry(USRebarSize barsize);
+	void SetBarSize(int barsize);
+	void SetGeometry();
+	void SetMaterial(Steel* pSteelType) { m_pSteel = pSteelType; }
 
 	// gets
 	double GetDiameter() { return m_Diameter; }
