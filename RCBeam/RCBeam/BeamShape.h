6044e8994b77956ce8ee19f9f2718ea9710ed82d
m-stone <stone.ma@gmail.com>
2021-12-01 14:57:16 -0500

    Update BeamShape.h
    Update pointers

--- RCBeam/RCBeam/BeamShape.h
+++ RCBeam/RCBeam/BeamShape.h
@@ -1,27 +1,28 @@
 #pragma once
 #include "pch.h"
-#include "Concrete.h"
 
 class BeamShape
 {
 protected:
 	double m_Area_gross, m_Ixx, m_Iyy;
 	double m_Length;
-	Concrete* m_pConcrete;
+	//Concrete* m_pConcrete;
+	std::shared_ptr<Concrete> m_pConcrete;
 
 public:
 
 	// Sets
-	virtual void SetAreaGross() = 0;
-	virtual void SetInertia() = 0;
-	virtual void SetLength(double length) { m_Length = length; }
-	virtual void SetMaterial(Concrete* pConcrete) { m_pConcrete = pConcrete; }
+	virtual void setAreaGross() = 0;
+	virtual void setInertia() = 0;
+	virtual void setLength(double length) { this->m_Length = length; }
+	//virtual void setMaterial(Concrete* pConcrete) { this->m_pConcrete = pConcrete; }
+	virtual void setMaterial(std::shared_ptr<Concrete>& pConcrete) { this->m_pConcrete = pConcrete; }
 
 	// Gets
-	double GetAreaGross() { return m_Area_gross; }
-	double GetIxx() { return m_Ixx; }
-	double GetIyy() { return m_Iyy; }
-	double GetLength() { return m_Length; }
+	double getAreaGross() { return m_Area_gross; }
+	double getIxx() { return m_Ixx; }
+	double getIyy() { return m_Iyy; }
+	double getLength() { return m_Length; }
 
 };
 
